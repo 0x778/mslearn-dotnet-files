aa3af7eb2b2323101cf1cdb3786fba93b763d34d
00x78 <hussamalmaswari@gmail.com>
2024-04-15 01:28:53 +0300

    Add some way of Get the path

--- Program.cs
+++ Program.cs
@@ -1,13 +1,21 @@
 ﻿using System.IO;
 using System.Collections.Generic;
-
-
+//Get specifec File that is inside The funcation
 var salesFiles = FindFiles("stores");
-    
+
 foreach (var file in salesFiles)
 {
     Console.WriteLine(file);
 }
+//Get current path
+Console.WriteLine(Directory.GetCurrentDirectory());
+string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+////////////////////////////////////////////////////////////////////////////
+Console.WriteLine($"stores{Path.DirectorySeparatorChar}201");
+
+// returns:
+// stores\201 on Windows
+// stores/201 on macOS
 
 IEnumerable<string> FindFiles(string folderName)
 {
